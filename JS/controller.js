e0cd0c17c7e1b7e27154e1f5d200e5661ede57c9
Pham Anh Viet <141446249+AnhViet45@users.noreply.github.com>
2023-11-04 14:59:38 +0700

    Update controller.js
    Change  the sorting algo

--- JS/controller.js
+++ JS/controller.js
@@ -47,37 +47,34 @@ function renderProductList(productArr) {
     };
   }
 
+
   function Sorting(productArr) {
     var contentHTML = "";
-    for (var i = 0; i < productArr.length-1; i++) {
-        for(var j=i+1; j < productArr.length; j++){
-            if(productArr[j].price < productArr[i].price){
-                let product = array[i];
-                array[i] = array[j];
-                array[j] = product;
-                var trString = `
-                <tr>
-                            <td>${product.id}</td>
-                            <td>${product.name}</td>
-                            <td>${product.price}</td>
-                            <td>${product.img}</td>
-                            <td>${product.desc}</td>
-                            <td>
-            
-                                    <button
-                                    onclick=editProduct(${product.id})
-                                    class="btn btn-warning">Edit</button>
-            
-                                    <button
-                                    onclick=deleteProduct(${product.id})
-                                    class="btn btn-danger">Delete</button>
-            
-                            </td>
-                </tr>
-                `;
-                contentHTML += trString;
-            }
-        }
+    productArr.sort((a, b) =>a.price - b.price);
+    for (var i = 0; i < productArr.length; i++) {
+      var product = productArr[i];
+      var trString = `
+      <tr>
+                  <td>${product.id}</td>
+                  <td>${product.name}</td>
+                  <td>${product.price}</td>
+                  <td>${product.img}</td>
+                  <td>${product.desc}</td>
+                  <td>
+  
+                          <button
+                          onclick=editProduct(${product.id})
+                          class="btn btn-warning">Edit</button>
+  
+                          <button
+                          onclick=deleteProduct(${product.id})
+                          class="btn btn-danger">Delete</button>
+  
+                  </td>
+                  
+      </tr>
+      `;
+      contentHTML += trString;
     }
     document.getElementById("tblDanhSachSP").innerHTML = contentHTML;
   }
