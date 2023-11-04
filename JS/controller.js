34276c22ca28ff428adf8211cb91a44ad5dec4e1
Pham Anh Viet <141446249+AnhViet45@users.noreply.github.com>
2023-11-04 15:34:05 +0700

    Update controller.js

--- JS/controller.js
+++ JS/controller.js
@@ -48,9 +48,40 @@ function renderProductList(productArr) {
   }
 
 
-  function Sorting(productArr) {
+  function AsSorting(productArr) {
     var contentHTML = "";
     productArr.sort((a, b) =>a.price - b.price);
+    for (var i = 0; i < productArr.length; i++) {
+      var product = productArr[i];
+      var trString = `
+      <tr>
+                  <td>${product.id}</td>
+                  <td>${product.name}</td>
+                  <td>${product.price}</td>
+                  <td>${product.img}</td>
+                  <td>${product.desc}</td>
+                  <td>
+  
+                          <button
+                          onclick=editProduct(${product.id})
+                          class="btn btn-warning">Edit</button>
+  
+                          <button
+                          onclick=deleteProduct(${product.id})
+                          class="btn btn-danger">Delete</button>
+  
+                  </td>
+                  
+      </tr>
+      `;
+      contentHTML += trString;
+    }
+    document.getElementById("tblDanhSachSP").innerHTML = contentHTML;
+  }
+
+  function DesSorting(productArr) {
+    var contentHTML = "";
+    productArr.sort((a, b) =>b.price - a.price);
     for (var i = 0; i < productArr.length; i++) {
       var product = productArr[i];
       var trString = `
