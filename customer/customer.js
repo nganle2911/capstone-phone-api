a5c6b3aafad917086f60e5e325aada60cfae5ee8
Ngan LE <ngan.ltkn2911@gmail.com>
2023-11-13 23:51:39 +0100

    executed features in shopping cart

--- customer/customer.js
+++ customer/customer.js
@@ -5,16 +5,17 @@ import { CartProduct } from "./controller/cartProduct.js";
 const getEle = (id) => document.getElementById(id);
 
 let service = new Service();
-let cart= [];
+let cart = [];
 
 
-// Function display the list of products
+// todo: Render list of products on UI - customer page 
 const renderList = (data) => {
   let content = "";
   data.forEach((ele) => {
     // id += 1;
     content += `
-        <div class="card col-3">
+      <div class="card-item col-3 mt-4">
+        <div class="card">
             <img
             src="${ele.product.img}"
             class="img-fluid py-5"
@@ -32,26 +33,11 @@ const renderList = (data) => {
               <div class="d-flex justify-content-between">
                 <span>$${ele.product.price}  </span>
                 <button class="btn btn-success ml-2" id="btnAdd${ele.product.id}" onclick="addProduct(${ele.product.id})">ADD</button>
-
-                <div class="bg-dark" style="color:white; display:none;" id="qty_${ele.product.id}">
-                  <div class="item-row flus-minus d-flex align-items-center">
-                    <a class="btn add-btn d-flex align-items-center" onclick="decrQty('${ele.product.id}')">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-                        <path d="M192 448c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25l160-160c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25L77.25 256l137.4 137.4c12.5 12.5 12.5 32.75 0 45.25C208.4 444.9 200.2 448 192 448z" />
-                      </svg>
-                    </a>
-                    <span id="qtyItem_${ele.product.id}">${ele.quantity}</span>
-                    <a class="btn minus-btn d-flex align-items-center" onclick="incrQty('${ele.product.id}')">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-                        <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-                      </svg>
-                    </a>
-                  </div>
-                </div>
               </div>
             </div>
         </div>
-        `;
+      </div>
+    `;
   });
   getEle("listSP").innerHTML = content;
 };
---
+++ customer/index.js
@@ -0,0 +1,215 @@
+import { Service } from "./controller/Service.js";
+
+let productsList = [];
+let cart = [];
+let service = new Service(); 
+
+const getEle = (id) => document.getElementById(id);
+
+// todo: Render list of products on UI - customer page 
+const renderList = () => {
+    let content = "";
+    if (productsList.length > 0) {
+        productsList.forEach((ele) => {
+            // id += 1;
+            content += `
+            <div class="card-item col-3 mt-4">
+              <div class="card">
+                  <img
+                  src="${ele.img}"
+                  class="img-fluid py-5"
+                  style="width: 15.625em"
+                  alt=""
+                  />
+                  <div class="card-body" id="card_body${ele.id}">
+                    <h3 class="card-title">${ele.name}</h3>
+                    <div class="card-text pb-4">
+                       <span class="descText">Camera sau:</span> ${ele.backCamera} <br />
+                        <span class="descText">Camera trước: </span>${ele.frontCamera} <br />
+                        <span class="descText">Màn hình: </span>${ele.screen} <br />
+                        <span class="descText">Là một sản phẩm có </span>${ele.desc}
+                    </div>
+                    <div class="d-flex justify-content-between">
+                      <span>$${ele.price}</span>
+                      <button class="btn btn-success ml-2" onclick="addToCart(${ele.id})">ADD</button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          `;
+        });
+
+        // render on UI 
+        getEle("listSP").innerHTML = content;
+    }
+};
+
+// todo: Call api to get list of products and render on UI
+const getListApi = () => {
+    service.getListAPI().then((result) => {
+        productsList = result.data; 
+        console.log("productsList", productsList);
+
+        // render products on UI
+        renderList(); 
+
+        // get cart from localStorage
+        getCartFromLocalStorage();
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+
+// todo: add product to cart by clicking on "add" button on each product => product will be added to cart
+window.addToCart = (product_id) => {
+    // find the position of this item in cart array => to add quantity if product_id is the same 
+    let itemPositionInCart = cart.findIndex((value) => {
+        console.log("value", value);
+        return value.product_id === product_id; 
+    });
+    console.log("ItemPositionInCart", itemPositionInCart);
+
+    // if cart array is empty => add item to cart
+    if (cart.length <= 0) {
+        cart = [{
+            product_id: product_id,
+            quantity: 1
+        }]
+    } else if (itemPositionInCart < 0) {
+        // if cart has already items, and the item added does not exist in cart => push it in cart
+        cart.push({
+            product_id: product_id,
+            quantity: 1
+        })
+    } else {
+        // if this item already existed in cart => quantity of this item +1 
+        cart[itemPositionInCart].quantity++; 
+    }
+
+    console.log("cart after adding items", cart);
+    
+    // render cart on UI 
+    renderCart();
+    saveCartToLocalStorage(); 
+}
+
+// todo: render cart on UI
+const renderCart = () => {
+    let listCartHTML = "";
+    let totalQuantity = 0;
+    let totalAmount = 0; 
+
+    if (cart.length > 0) {
+        console.log("cart", cart);
+        cart.map((itemCart) => {
+            console.log("itemCart", itemCart);
+            totalQuantity += itemCart.quantity; 
+
+            console.log("productsList", productsList);
+            // search in productsList array, get product.id == itemCart.product_id
+            let product = productsList.find((product) => {
+                return product.id == itemCart.product_id; 
+            });
+            console.log("product", product);
+
+            let amount = itemCart.quantity * product.price; 
+            totalAmount += amount; 
+
+            listCartHTML += `
+                <tr id=item_${product.id}>
+                    <td><img src=${product.img} alt="..." width="100px" /></td>
+                    <td>
+                    <h6 class="text-muted">${product.type}</h6>
+                    <h6 class="text-black mb-0">${product.name}</h6>
+                    </td>
+                    <td>$${product.price}</td>
+                    <td>
+                    <div class="btn_quantity">
+                        <span id="minus" onclick="decreaseQuantity(${product.id})"><i class="fa fa-minus"></i></span>
+                        <span>${itemCart.quantity}</span>
+                        <span id="plus" onclick="increaseQuantity(${product.id})"><i class="fa fa-plus"></i></span>
+                    </div>
+                    </td>
+                    <td>$${amount}</td>
+                    <td><i class="fa fa-trash-alt" onclick="removeItemInCart(${product.id})"></i></td>
+                </tr>
+            `;
+        })
+    }
+
+    getEle("listCart").innerHTML = listCartHTML; 
+    getEle("totalAmount").innerText = totalAmount; 
+}
+
+// todo: increase quantity of item in cart
+window.increaseQuantity = (product_id) => {
+    // find the position of item in cart
+    let itemPositionCart = cart.findIndex((item) => {
+        return item.product_id == product_id;
+    });
+    console.log("itemPositionCart", itemPositionCart);
+
+    if (itemPositionCart >= 0) {
+        cart[itemPositionCart].quantity++; 
+    }
+
+    renderCart();
+    saveCartToLocalStorage();
+}
+
+// todo: decrease quantity of item in cart 
+window.decreaseQuantity = (product_id) => {
+    
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    
+
+    if (positionItemInCart >= 0) {
+        let itemQuan = cart[positionItemInCart].quantity - 1;
+
+        if (itemQuan > 0) {
+            cart[positionItemInCart].quantity = itemQuan;
+        } else {
+            cart.splice(positionItemInCart, 1);
+        }
+    }
+
+    renderCart();
+    saveCartToLocalStorage();
+}
+
+// todo: remove item from cart
+window.removeItemInCart = (product_id) => {
+    // remove item on UI
+    document.getElementById(`item_${product_id}`).remove();
+
+    // remove item in localStorage 
+    let positionItemInCart = cart.findIndex((value) => value.product_id == product_id);
+    cart.splice(positionItemInCart, 1);
+
+    renderCart();
+    saveCartToLocalStorage();
+}
+
+// Display the list of products
+getListApi();
+
+// todo: save cart to localStorage 
+const saveCartToLocalStorage = () => {
+    // convert data to json format
+    let cartJson = JSON.stringify(cart); 
+    // save to localStorage
+    localStorage.setItem("CART_LOCAL", cartJson);
+}
+
+// todo: get cart from localStorage
+const getCartFromLocalStorage = () => {
+    // get cartJson saved in localStorage
+    let cartJson = localStorage.getItem("CART_LOCAL", cart);
+    // Check if cartJson is null => don't have data => can't parse 
+    // Check if cartJson is not null => parse data 
+    if (cartJson != null) {
+        // convert cartJson to cart array
+        cart = JSON.parse(cartJson);
+        renderCart();
+    }
+}
